b072c554477f256048d9aeae098768fa187ad2e2
Alexandre Rio <contact@alexrio.fr>
2017-10-11 10:24:56 +0200

    Fix anefe method, add certes method

--- sopel/modules/anefe.py
+++ sopel/modules/anefe.py
@@ -2,9 +2,13 @@
 from sopel import module
 
 @module.rule('.*en effet.*')
-def anefe(bot, trigger):
+def eneffet(bot, trigger):
   bot.say('anéfé')
 
 @module.rule('.*anéfé.*')
 def anefe(bot, trigger):
   bot.say('certes')
+
+@module.rule('.*certes.*')
+def certes(bot, trigger):
+  bot.say('anéfé')
