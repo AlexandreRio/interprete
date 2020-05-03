418cff9ab901894eef94cd549f77276cc86aa1ec
Alexandre Rio <contact@alexrio.fr>
2020-05-03 17:43:51 +0200

    fix fucking weather parsing

--- sopel/modules/fuckingweather.py
+++ sopel/modules/fuckingweather.py
@@ -7,25 +7,23 @@ Licensed under the Eiffel Forum License 2.
 
 http://sopel.chat
 """
-from sopel.module import commands, rate, priority, NOLIMIT
+from sopel.module import commands, rate, priority
 from sopel import web
 import re
 
 
 @commands('fucking_weather', 'fw')
-@rate(30)
-@priority('low')
 def fucking_weather(bot, trigger):
-    text = trigger.group(2)
-    if not text:
-        bot.reply("INVALID FUCKING PLACE. PLEASE ENTER A FUCKING ZIP CODE, OR A FUCKING CITY-STATE PAIR.")
-        return
-    text = web.quote(text)
-    page = web.get("http://thefuckingweather.com/Where/%s" % (text))
-    re_mark = re.compile('<p class="remark jsRemark">(.*?)</p>')
-    results = re_mark.findall(page)
-    if results:
-        bot.reply(results[0])
-    else:
-        bot.reply("I CAN'T GET THE FUCKING WEATHER.")
-        return bot.NOLIMIT
+  text = trigger.group(2)
+  if not text:
+    bot.reply("INVALID FUCKING PLACE. PLEASE ENTER A FUCKING ZIP CODE, OR A FUCKING CITY-STATE PAIR.")
+    return
+  text = web.quote(text)
+  page = web.get("http://thefuckingweather.com/Where/%s" % (text))
+  #re_mark = re.compile('<p class="remark jsRemark">(.*?)</p>')
+  re_mark = re.compile('<h1>(.*?)</h1>')
+  results = re_mark.findall(page)
+  if results:
+    bot.reply(results[0])
+  else:
+    bot.reply("I CAN'T GET THE FUCKING WEATHER.")
